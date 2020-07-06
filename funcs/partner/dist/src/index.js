6654d7f1e012dcb9e4e488d7956988186723cf43
Liam Murray <liammurray@users.noreply.github.com>
2020-07-06 12:10:50 -0700

    Remove dist

--- funcs/partner/dist/src/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const util_1 = require("./util");
-console.log('hola amigo');
-console.log(util_1.flatten([['a', 'b'], ['c']]));
-//# sourceMappingURL=index.js.map
--- funcs/partner/dist/src/util.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function flatten<T>(arr: T[][]): T[];
-export declare function swap<T>(arr: T[], first: number, second: number): T[];
